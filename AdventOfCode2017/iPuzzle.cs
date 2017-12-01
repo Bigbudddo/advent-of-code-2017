7656f95e62dda7dc308010bfdd509ed310e939a4
Stuart Harrison <email@stuart-harrison.com>
2017-12-01 15:32:30 +0000

    Beautified the code

---
+++ AdventOfCode2017/iPuzzle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017 {
+    
+    public interface iPuzzle {
+
+        void Part1();
+        void Part2();
+    }
+}
