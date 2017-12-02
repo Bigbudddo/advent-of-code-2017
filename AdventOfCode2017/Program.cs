db4328aad5db5a5d187e9ee8f28b1ef56b4098d5
Stuart Harrison <email@stuart-harrison.com>
2017-12-02 14:45:05 +0000

    Day 2 completed

--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -71,6 +71,8 @@ namespace AdventOfCode2017 {
             switch (day) {
                 case 1:
                     return new Day1();
+                case 2:
+                    return new Day2();
                 default:
                     return null;
             }
